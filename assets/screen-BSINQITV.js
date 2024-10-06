a737c8f76963af29e555a768a2907e82233e5665
KoenMW <KoenMW@users.noreply.github.com>
2024-10-06 21:22:44 +0000

    deploy: cfc4a7b04eb7ff69d3676be3d17ff7c75caa5df4

---
+++ assets/screen-BSINQITV.js
@@ -0,0 +1,1 @@
+import{P as e,g as a}from"./game-Ci-kKBqs.js";import"./dom-CIGCZKQE.js";import"./index-Cf2FFf-C.js";const t=document.querySelector(".screen"),c={type:e.WEBGL,parent:t,scale:{mode:e.Scale.FIT,width:512,height:256},zoom:4,scene:a,physics:{default:"arcade",arcade:{debug:!0}}},n=()=>{new e.Game(c)};export{n as default};
