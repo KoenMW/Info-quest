0ecc65f748ad91853b968f1fad9d6c1368d7c553
KoenMW <KoenMW@users.noreply.github.com>
2024-10-29 22:09:37 +0000

    deploy: 7047f4432eaf9353761e17ebd64a4188a7d9d76f

---
+++ assets/screen-DDO8o0T7.js
@@ -0,0 +1,1 @@
+import{P as e,g as a}from"./game-yfUpwCpW.js";import"./dom-Rqudda6X.js";import"./index-q7L-rRbd.js";const t=document.querySelector(".screen"),c={type:e.WEBGL,parent:t,scale:{mode:e.Scale.FIT,width:512,height:256},zoom:4,scene:a,physics:{default:"arcade",arcade:{debug:!0}}},n=()=>{new e.Game(c)};export{n as default};
