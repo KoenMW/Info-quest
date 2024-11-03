2165ae6b7fd288ce56b28e4e3564bec2b8762f12
KoenMW <KoenMW@users.noreply.github.com>
2024-11-03 23:35:31 +0000

    deploy: 5fe1cb12e1361a8d19631bd9a846af9598f12053

---
+++ assets/screen-BkkFmvYW.js
@@ -0,0 +1,1 @@
+import{P as e,g as t}from"./game-CbJn3r28.js";import{b as a}from"./index-u2_iaW_O.js";import"./index-CX5XSkDx.js";const o=document.querySelector(".screen"),s={type:e.WEBGL,parent:o,scale:{mode:e.Scale.FIT,width:a.width,height:a.height},zoom:4,scene:t,physics:{default:"arcade",arcade:{debug:!1}}},m=()=>{new e.Game(s)};export{m as default};
--- assets/screen-COj8bsSR.js
+++
@@ -1,1 +0,0 @@
-import{P as e,g as t}from"./game-B7Vjcq2s.js";import{b as a}from"./index-tzmyt3Ux.js";import"./index-BePMAZoG.js";const o=document.querySelector(".screen"),s={type:e.WEBGL,parent:o,scale:{mode:e.Scale.FIT,width:a.width,height:a.height},zoom:4,scene:t,physics:{default:"arcade",arcade:{debug:!1}}},m=()=>{new e.Game(s)};export{m as default};
