cee8d39d2e35bbfc9126cd662396bfd5ba73d2ea
KoenMW <KoenMW@users.noreply.github.com>
2024-11-11 21:22:18 +0000

    deploy: aebbf23f6ad7a46b7dad98e1c4287303295114cc

--- assets/screen-BwsDdM1U.js
+++
@@ -1,1 +0,0 @@
-import{P as e,g as t}from"./game-Cenqqdcu.js";import{b as a}from"./index-D0ue2Uqd.js";import"./index-BnUvRTZK.js";const o=document.querySelector(".screen"),s={type:e.WEBGL,parent:o,scale:{mode:e.Scale.FIT,width:a.width,height:a.height},zoom:4,scene:t,physics:{default:"arcade",arcade:{debug:!1}}},m=()=>{new e.Game(s)};export{m as default};
