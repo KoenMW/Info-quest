29dd143bed42f1565303fa72cf3ca0813fd73501
KoenMW <KoenMW@users.noreply.github.com>
2024-11-25 21:24:54 +0000

    deploy: c16c405150a283b7a510d80e1c73702323c0fa6a

---
+++ assets/screen-DeuARXKG.js
@@ -0,0 +1,1 @@
+import{P as e,g as t}from"./game-DgUCN0yN.js";import{b as a}from"./index-Q0fmcAX8.js";import"./index-B8X4IfGD.js";const o=document.querySelector(".screen"),s={type:e.WEBGL,parent:o,scale:{mode:e.Scale.FIT,width:a.width,height:a.height},zoom:4,scene:t,physics:{default:"arcade",arcade:{debug:!1}}},m=()=>{new e.Game(s)};export{m as default};
