788995763d252c722e0088a54fcee08b127f7193
KoenMW <KoenMW@users.noreply.github.com>
2025-03-24 20:32:32 +0000

    deploy: 2e2a9d01a59f21f267c7dbb42a33256c0884dbf0

--- assets/screen-C73jTkGO.js
+++
@@ -1,1 +0,0 @@
-import{P as e,g as t}from"./game-vSZNzz_P.js";import{b as a}from"./index-DJ2VrgAJ.js";import"./index-Duw8pkAR.js";const o=document.querySelector(".screen"),s={type:e.WEBGL,parent:o,scale:{mode:e.Scale.FIT,width:a.width,height:a.height},zoom:4,scene:t,physics:{default:"arcade",arcade:{debug:!1}}},m=()=>{new e.Game(s)};export{m as default};
